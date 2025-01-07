428bd1713d54f0de967a29a348a3775b45d6fdb2
aiidiot <150623748+aiidiot@users.noreply.github.com>
2025-01-07 21:42:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -408,4 +408,9 @@ document.getElementById('deleteTemplateBtn').addEventListener('click', function(
 window.addEventListener('DOMContentLoaded', function() {
     // Załaduj zapisane szablony do selecta
     loadSavedTemplates();
+     // Włącz cień domyślnie
+    const shadowToggle = document.getElementById('shadowToggle');
+    shadowToggle.checked = true;
+    shadow.style.display = 'block';
+    updateShadow();
 });
